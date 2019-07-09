df6d7f10ed56284c0cadadbc5d3e8625c70d217c
Colin <cstatro@gmail.com>
2019-07-09 13:01:51 -0400

    connection tested/working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
     has_many :ratings
     has_many :beers, through: :ratings
     def rate_beer(hash)
-        beer = Beer.create(name: hash[:name],style: hash[:style][:shortName],abv: hash[:abv])
+        beer = Beer.create(name: hash["name"],style: hash["style"]["shortName"],abv: hash["abv"])
         prompt_for_rating
         input = STDIN.gets.chomp
         rating = Rating.create(user: self,beer: beer, rating: input.to_f)  
