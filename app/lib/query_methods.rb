6c80523f58fe05292f88fe2fbec48098461d4082
github username <github email address>
2019-07-10 11:52:29 -0400

    worked on query methods to display total results, number or pages and current page of user search

--- app/lib/query_methods.rb
+++ app/lib/query_methods.rb
@@ -1,8 +1,8 @@
 
 
-def beer_search(arg)
-    beer = RestClient.get("http://api.brewerydb.com/v2/search?key=3f1a5ca2f9d684d046245ab5c02b0f47&q=#{arg}&withBreweries=y")
-    result_json = JSON.parse(beer)["data"]
+def beer_search(arg,pg=1)
+    beer = RestClient.get("http://api.brewerydb.com/v2/search?key=3f1a5ca2f9d684d046245ab5c02b0f47&q=#{arg}&withBreweries=y&p=#{pg}")
+    result_json = JSON.parse(beer)
 end
 
 
@@ -11,9 +11,15 @@ def beer_search_first(arg)
     beer_search(arg).find {|beer|beer}
 end
 
+def display_navigation(arg,pg=1)
+    puts "There are #{beer_search(arg,pg)["totalResults"]} total results. You are on page #{beer_search(arg,pg)["currentPage"]} of #{beer_search(arg,pg)["numberOfPages"]}. "
+    puts "---------------------------------------------------"
+end
+
 # puts a numbered list of search results, array of search results is returned
-def beer_search_with_index(arg)
-    beer_search(arg).each_with_index do |beer, index|
+def beer_search_with_index(arg,pg=1)
+    display_navigation(arg,pg)
+    beer_search(arg,pg)["data"].each_with_index do |beer, index|
         puts "#{index+1}. #{beer["name"]} - #{beer["breweries"].first["name"]}"
     end
 end
