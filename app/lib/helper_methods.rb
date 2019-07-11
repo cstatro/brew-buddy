94d56ab69e2a82c0159a6b3b153708a588774de1
Colin <cstatro@gmail.com>
2019-07-11 15:49:59 -0400

    seeding data base

--- app/lib/helper_methods.rb
+++ app/lib/helper_methods.rb
@@ -7,8 +7,6 @@ def limit_rating(num)
         num
     end
 end
-
-
 #### test method
 def colin 
     User.find(1)
@@ -18,14 +16,12 @@ def joe
     User.find(2)
 end
 ### Paginate_Toggling
-
 def page_toggle_up(current,max)
     current == max ? current : current += 1
 end
 def page_toggle_down(current)
     current == 1 ? current : current -= 1
 end
-
 ### Stats helper
 def find_favorite_type(beer_arr)
     beer_types = beer_arr.pluck(:style)
